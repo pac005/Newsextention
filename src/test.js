b1279adee755b308ad755314d059a8123019b0d8
pac005 <71997107+pac005@users.noreply.github.com>
2020-11-22 10:46:58 -0500

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,19 @@
+// style testing without using API calls every refresh :)
+
+// for(var i=0; i < 10; i++){
+
+//             const card = document.createElement('div')
+//             card.setAttribute('class','card')
+
+//             const h1 = document.createElement('h1')
+//             h1.textContent = "Testing Title"
+
+//             const p = document.createElement('p')
+//             //data.articles[a].description = data.articles[a].description.substring(0,300) //limit 300 char
+//             p.textContent = "Testing if the text actually shows up on the tile. Some more extra text to check if it prints"
+
+//             container.appendChild(card)
+
+//             card.appendChild(h1)
+//             card.appendChild(p)
+// }
